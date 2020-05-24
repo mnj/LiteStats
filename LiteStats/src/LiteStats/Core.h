9c0450d4abd654d8692b428a66507213da52e2f7
Michael N. Jensen <mij@dustin.dk>
2020-05-24 19:19:50 +0200

    Enable linux (gcc) support

--- LiteStats/src/LiteStats/Core.h
+++ LiteStats/src/LiteStats/Core.h
@@ -6,6 +6,12 @@
     #else
         #define LS_API __declspec(dllimport)
     #endif
+#elif defined(__GNUC__)
+    #ifdef LS_BUILD_DLL
+        #define LS_API __attribute__((visibility("default")))
+    #else
+        #define LS_API
+    #endif
 #else
     #error LiteStats currently only support one platform (Windows)
 #endif
